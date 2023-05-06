592494cc45d285f05fb50ffe39685fee3369a18f
Tushar Subhra <tusharsubhraios@gmail.com>
2023-05-07 00:19:47 +0530

    footer change

--- vendor/tcg/voyager/resources/views/partials/app-footer.blade.php
+++ vendor/tcg/voyager/resources/views/partials/app-footer.blade.php
@@ -1,10 +1,8 @@
 <footer class="app-footer">
     <div class="site-footer-right">
-        @if (rand(1,100) == 100)
-            <i class="voyager-rum-1"></i> {{ __('voyager::theme.footer_copyright2') }}
-        @else
-            {!! __('voyager::theme.footer_copyright') !!} <a href="http://thecontrolgroup.com" target="_blank">The Control Group</a>
-        @endif
+    
+        <a href="http://appzone.in/" target="_blank">appzone</a>
+       
         @php $version = Voyager::getVersion(); @endphp
         @if (!empty($version))
             - {{ $version }}
