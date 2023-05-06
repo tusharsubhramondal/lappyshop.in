fbb1a53d6b74df3ad03ae75fb7fabcc2c79f2417
Tushar Subhra <tusharsubhraios@gmail.com>
2023-05-07 00:21:21 +0530

    modified

--- vendor/tcg/voyager/resources/views/partials/app-footer.blade.php
+++ vendor/tcg/voyager/resources/views/partials/app-footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="app-footer">
     <div class="site-footer-right">
     
-        <a href="http://appzone.in/" target="_blank">appzone</a>
+        Powered by <a href="http://appzone.in/" target="_blank">appzone</a>
        
         @php $version = Voyager::getVersion(); @endphp
         @if (!empty($version))
